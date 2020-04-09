2ab09269561e44bef83fc4862de0b6aa51836e53
Maryna Andreieva <andreieva.maryna@gmail.com>
2020-04-09 14:20:56 +0300

    Made Separation of Scenarios. Added @Before and @After.

--- src/test/java/GetPetTest.java
+++ src/test/java/GetPetTest.java
@@ -1,24 +1,86 @@
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.response.ValidatableResponse;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.is;
 
 public class GetPetTest {
 
-    @Test
-    public void getPetById(){
-        int id = 8888888;
+        long createdPetId;
+
+        @Before
+        public void before2() {
+            RequestSpecBuilder spec = new RequestSpecBuilder();
+            spec.setBaseUri("https://petstore.swagger.io/v2");
+            spec.addHeader("Content-Type", "application/json");
+            RestAssured.requestSpecification = spec.build();
+        }
+
+        @Before
+        public void before1() {
+            int id = 0;
+            String body = "{\n" +
+                    "  \"id\": \""+ id +"\",\n" +
+                    "  \"category\": {\n" +
+                    "    \"id\": 0,\n" +
+                    "    \"name\": \"string\"\n" +
+                    "  },\n" +
+                    "  \"name\": \"Bobik\",\n" +
+                    "  \"photoUrls\": [\n" +
+                    "    \"string\"\n" +
+                    "  ],\n" +
+                    "  \"tags\": [\n" +
+                    "    {\n" +
+                    "      \"id\": 0,\n" +
+                    "      \"name\": \"string\"\n" +
+                    "    }\n" +
+                    "  ],\n" +
+                    "  \"status\": \"available\"\n" +
+                    "}";
+            ValidatableResponse response = given()
+                    .log()
+                    .all()
+                    .body(body)
+                    .when()
+                    .post("/pet")
+                    .then()
+                    .log()
+                    .all()
+                    .statusCode(200);
+
+            createdPetId = response.extract().path("id");
+            System.out.println(createdPetId);
+        }
+
+        @Test
+        public void getPetById(){
         given()
-                .log().all()
-                .baseUri("https://petstore.swagger.io")
+                .log()
+                .all()
                 .when()
-                .get("/v2/pet/{id}", id)
+                .get("/pet/{id}", createdPetId)
                 .then()
-                .log().all()
+                .log()
+                .all()
+                .body("id", anyOf(is(createdPetId), is("available")))
                 .statusCode(200);
     }
-
+       @After
+        public void deleteTest() {
+            given()
+                    .log()
+                    .all()
+                    .when()
+                    .delete("/pet/{id}", createdPetId)
+                    .then()
+                    .log()
+                    .all()
+                    .body("message", is(String.valueOf(createdPetId)))
+                    .statusCode(200);
+        }
 }
-
-
-//implement 5 new tests per each endpoint for Pet entity
-//also commit/push your hometask to git repo
